c026d3fdb2cbe730032130a6e566bac1875d8703
kanikaramsejal@gmail.com <kanikaramsejal@gmail.com>
2020-08-01 08:44:58 -0700

    Firebase

---
+++ Pods/Target Support Files/Pods-EduCare-EduCareUITests/Pods-EduCare-EduCareUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EduCare_EduCareUITests : NSObject
+@end
+@implementation PodsDummy_Pods_EduCare_EduCareUITests
+@end
---
+++ Pods/Target Support Files/Pods-EduCare/Pods-EduCare-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EduCare : NSObject
+@end
+@implementation PodsDummy_Pods_EduCare
+@end
